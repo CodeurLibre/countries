b61befbf4e224bda0af8e731b52ca7a0d04c81dc
Codeur Libre <librecodeur@gmail.com>
2023-03-04 17:59:09 +0000

    Countries projet terminé

---
+++ src/pages/About.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+        perferendis placeat tempore quos adipisci, repudiandae, minus corporis
+        sint aut commodi nihil sapiente sunt, est quidem illum pariatur facilis
+        asperiores dolore quas quam corrupti minima? Ea voluptate facere id
+        commodi, maiores ex ullam blanditiis fugit incidunt minus esse labore
+        praesentium? Iure aspernatur doloribus harum minima ipsa ut eligendi
+        pariatur fuga, atque, odio porro, nobis totam dolorum nemo inventore
+        veniam optio at illum quae velit eveniet alias tenetur! Cum
+        reprehenderit saepe eveniet ut dolorum accusantium fuga tempore
+        molestias dignissimos nam quidem officia facilis error sit possimus
+        culpa, obcaecati .
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt quia
+        aspernatur illum delectus exercitationem corrupti quo doloremque
+        suscipit provident. Maiores molestiae enim minima dicta debitis vitae
+        perferendis voluptatem tenetur maxime temporibus dolor, repellat
+        voluptatibus nam quibusdam illo eius velit. Sapiente consequuntur
+        similique nemo voluptatum commodi.
+      </p>
+    </div>
+  );
+};
+
+export default About;
